f1813338e996b61a036e764ccb0b400e075a88e6
allmeste <90242419+allmeste@users.noreply.github.com>
2023-02-01 18:40:47 +0300

    Add files via upload

---
+++ лаба11/лаба100/лаба100/лаба100.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+void main()
+
+{
+    setlocale(LC_CTYPE, "Russian");
+    using namespace std;
+    char tmp[33];
+    short int i; 
+    short  a;
+    short int mask = 16;  
+    short int num = 0;  
+    cout << "Введите число А " << endl;
+    cin >> a;
+    _itoa_s(a, tmp, 2);
+    cout <<"Число А в двоичной системе: "<< tmp << endl;
+    for (i = 0; i <= 5; i++)
+    { 
+         
+        if ((mask & a) == mask)
+        {
+            num++;
+        }
+        mask = mask << 1;
+    }
+    cout <<"Количество единиц с 5 по 10 бит:   "<< num << endl;
+}
